5bcabae998c24e31664e4112a7d5edcaa44597c5
B303-xx <administrador@b303-25.local>
2017-05-04 22:48:35 -0500

    Inicio del proyecto

---
+++ DemoRepoIOS/DemoRepoIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoRepoIOS
+//
+//  Created by B303-25 on 4/05/17.
+//  Copyright © 2017 B303-24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
